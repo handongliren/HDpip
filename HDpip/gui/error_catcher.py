9f02ab16e05a415ca23bf75abb542af62b520839
handongliren <1079489986@qq.com>
2026-06-24 08:02:16 +0800

    refactor(error_catcher): enhance error catching decorator with auto_close parameter

--- HDpip/gui/error_catcher.py
+++ HDpip/gui/error_catcher.py
@@ -15,7 +15,7 @@ import sys
 
 base_dir = pathlib.Path(__file__).parents[1].resolve()
 
-def catch(func: Callable[..., Any]):
+def catch(func: Callable[..., Any] | None = None, *, auto_close: int = 0):
     """
     装饰器，用于捕捉错误并显示对话框。
 
@@ -25,18 +25,27 @@ def catch(func: Callable[..., Any]):
     :rtype: _Wrapped[Callable[..., Any], Any, Callable[..., Any], Any | None]
     """
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-        except Exception as error:
-            result = None
-            subprocess.Popen(
-                [sys.executable, str(base_dir / "gui/error_dialog.py"), "".join(traceback.format_exception(error))],
-                stdin = subprocess.DEVNULL,
-                stdout = subprocess.DEVNULL,
-                stderr = subprocess.DEVNULL,
-                creationflags = subprocess.CREATE_NO_WINDOW if sys.platform == "win32" else 0
-            )
-        return result
-    return wrapper
+    def _decorate(f: Callable[..., Any], *, auto_close: int = -1):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as error:
+                auto_close = auto_close
+                auto_close_arg = ["--auto-close-auto_close", str(auto_close)] if auto_close and auto_close >= 0 else []
+
+                subprocess.Popen(
+                    [sys.executable, str(base_dir / "gui/error_dialog.py"), "--text", "".join(traceback.format_exception(error))] + auto_close_arg,
+                    stdin = subprocess.DEVNULL,
+                    stdout = subprocess.DEVNULL,
+                    stderr = subprocess.DEVNULL,
+                    creationflags = subprocess.CREATE_NO_WINDOW if sys.platform == "win32" else 0
+                )
+                return None
+
+        return wrapper
+
+    if func is None:
+        return lambda f: _decorate(f, auto_close = auto_close)
+    else:
+        return _decorate(func, auto_close = auto_close)

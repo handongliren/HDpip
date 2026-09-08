7be0523824eb2d11720c9629dea014eb7b54ae5c
handongliren <1079489986@qq.com>
2026-09-08 21:55:13 +0800

    feat: 添加镜像画布和语言渲染功能，更新中文语言文件，新增切换按钮图标

--- HDpip/gui/main.py
+++ HDpip/gui/main.py
@@ -75,6 +75,11 @@ class ControlCanvas(custom.containers.Canvas):
         渲染语言。
         """
 
+        self.install_button.set(self.data_manager.language["main"]["install_button"])
+        self.uninstall_button.set(self.data_manager.language["main"]["uninstall_button"])
+        self.upgrade_button.set(self.data_manager.language["main"]["upgrade_button"])
+        self.setting_button.set(self.data_manager.language["main"]["setting_button"])
+
     @override
     def __init__(self, master: maliang.Canvas | maliang.core.virtual.Widget | maliang.Tk | maliang.Toplevel, data_manager: core.data.DataManager = core.data.data_manager):
         """
@@ -86,10 +91,42 @@ class ControlCanvas(custom.containers.Canvas):
 
         super().__init__(master, expand = "xy", auto_zoom = True, auto_update = True, data_manager = data_manager)
 
-        self.install_button = custom.widgets.Button(self, ss((40, 40)), ss((120, 60)), theme = "outline-success", text = self.data_manager.language["main"]["install_button"], icon = bi("box-seam"), fontsize = ss(30))
-        self.uninstall_button = custom.widgets.Button(self, ss((190, 40)), ss((120, 60)), theme = "outline-danger", text = self.data_manager.language["main"]["uninstall_button"], icon = bi("trash"), fontsize = ss(30))
-        self.upgrade_button = custom.widgets.Button(self, ss((40, 140)), ss((120, 60)), theme = "outline-primary", text = self.data_manager.language["main"]["upgrade_button"], icon = bi("arrow-up"), fontsize = ss(30))
-        self.setting_button = custom.widgets.Button(self, ss((190, 140)), ss((120, 60)), theme = "outline-secondary", text = self.data_manager.language["main"]["setting_button"], icon = bi("gear"), fontsize = ss(30))
+        self.install_button = custom.widgets.Button(self, ss((40, 40)), ss((120, 60)), theme = "outline-success", icon = bi("box-seam"), fontsize = ss(30))
+        self.uninstall_button = custom.widgets.Button(self, ss((190, 40)), ss((120, 60)), theme = "outline-danger", icon = bi("trash"), fontsize = ss(30))
+        self.upgrade_button = custom.widgets.Button(self, ss((40, 140)), ss((120, 60)), theme = "outline-primary", icon = bi("arrow-up"), fontsize = ss(30))
+        self.setting_button = custom.widgets.Button(self, ss((190, 140)), ss((120, 60)), theme = "outline-secondary", icon = bi("gear"), fontsize = ss(30))
+
+        self.renderLanguage()
+
+class MirrorCanvas(custom.containers.Canvas):
+    """
+    镜像画布。
+    """
+
+    @override
+    def renderLanguage(self) -> None:
+        """
+        渲染语言。
+        """
+
+        self.title.set(self.data_manager.language["main"]["mirror"])
+        self.switch_button.set(self.data_manager.language["main"]["switch"])
+
+    @override
+    def __init__(self, master: maliang.Canvas | maliang.core.virtual.Widget | maliang.Tk | maliang.Toplevel, data_manager: core.data.DataManager = core.data.data_manager):
+        """
+        :param master: 父控件
+        :type master: maliang.Canvas | maliang.core.virtual.Widget | maliang.Tk | maliang.Toplevel
+        :param data_manager: 数据管理器
+        :type data_manager: core.data.DataManager
+        """
+
+        super().__init__(master, expand = "xy", auto_zoom = True, auto_update = True, data_manager = data_manager)
+
+        self.create_rectangle(ss((0, 0, 350, 50)), fill = custom.color.primary, outline = "")
+        self.title = maliang.Text(self, ss((10, 0)), None, anchor = "nw", fontsize = ss(30))
+        self.title.style.set(fg = custom.color.white)
+        self.switch_button = custom.widgets.Button(self, ss((340, 25)), ss((100, 30)), anchor = "e", theme = "outline-light", icon = bi("toggles"), fontsize = ss(20))
 
         self.renderLanguage()
 
@@ -120,19 +157,23 @@ class Main(custom.containers.Tk):
 
         self.base_canvas = maliang.Canvas(self, expand = "xy", auto_zoom = True, auto_update = True)
         self.base_canvas.place(x = 0, y = 0, width = ss(1200), height = ss(800))
+
         self.about_canvas = AboutCanvas(self.base_canvas, data_manager)
         self.about_canvas.place(x = 0, y = 0, width = ss(350), height = ss(150))
-        self.about_canvas.create_line(ss((0, 149, 350, 149)), width = 1, fill = custom.color.gray_500)
+        self.about_canvas.create_line(ss((0, 150, 350, 150)), width = 2, fill = custom.color.gray_500)
 
         self.control_canvas = ControlCanvas(self.base_canvas, data_manager)
         self.control_canvas.place(x = 0, y = ss(150), width = ss(350), height = ss(240))
-        self.control_canvas.create_line(ss((0, 240, 350, 240)), width = 1, fill = custom.color.gray_500)
-        self.base_canvas.create_line(ss((0, 390, 350, 390)), width = 1, fill = custom.color.gray_500)
+        self.control_canvas.create_line(ss((0, 240, 350, 240)), width = 2, fill = custom.color.gray_500)
 
-        self.base_canvas.create_line(ss((350, 0, 350, 800)), width = 1, fill = custom.color.gray_500)
-        self.base_canvas.create_line(ss((700, 0, 700, 800)), width = 1, fill = custom.color.gray_500)
-        self.base_canvas.create_line(ss((0, 540, 350, 540)), width = 1, fill = custom.color.gray_500)
-        self.base_canvas.create_line(ss((700, 400, 1200, 400)), width = 1, fill = custom.color.gray_500)
+        self.mirror_canvas = MirrorCanvas(self.base_canvas, data_manager)
+        self.mirror_canvas.place(x = 0, y = ss(390), width = ss(350), height = ss(160))
+        self.mirror_canvas.create_line(ss((0, 159, 350, 159)), width = 2, fill = custom.color.gray_500)
+
+        self.base_canvas.create_line(ss((350, 0, 350, 800)), width = 2, fill = custom.color.gray_500)
+        self.base_canvas.create_line(ss((700, 0, 700, 800)), width = 2, fill = custom.color.gray_500)
+        self.base_canvas.create_line(ss((0, 540, 350, 540)), width = 2, fill = custom.color.gray_500)
+        self.base_canvas.create_line(ss((700, 400, 1200, 400)), width = 2, fill = custom.color.gray_500)
         self.renderLanguage()
 
 if __name__ == "__main__":
